a8cf8f41dbfcf25f29164ac45dae57560a048642
Tyler K <tkelly10@my.wctc.edu>
2020-01-20 23:03:27 -0600

    Assignment complete

--- Program.cs
+++ Program.cs
@@ -1,24 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace ModasSeedData
 {
-    public class Program
+    class MainClass
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            // first create Locations list
+            List<Location> Locations = new List<Location>()
+            {
+                //new Location { LocationId = 1, Name = "Front Door"},
+                // TODO: Add locations
+                new Location {LocationId = 1, Name = "Front Door"},
+                new Location {LocationId = 2, Name = "Back Door"},
+                new Location {LocationId = 3, Name = "Living Room"}
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            };
+            // create date object containing current date/time
+            DateTime localDate = DateTime.Now;
+            // TODO: subtract 6 months from date
+            DateTime eventDate = localDate.AddMonths(-6);
+            // TODO: instantiate Random class
+            Random ran = new Random();
+            // TODO: create list to store events (Events)
+            List<Event> events = new List<Event>();
+            // loop for each day in the range from 6 months ago to today
+            while (eventDate < localDate)
+            {
+                int num = ran.Next(0, 6);
+
+                SortedList<DateTime, Event> dailyEvent = new SortedList<DateTime, Event>();
+
+
+                for (int i = 0; i < num; i++)
+                {
+                    int hour = ran.Next(0, 24);
+                    int min = ran.Next(0, 60);
+                    int sec = ran.Next(0, 60);
+                    int location = ran.Next(0, Locations.Count);
+
+                    DateTime d = new DateTime(eventDate.Year, eventDate.Month, eventDate.Day, hour, min, sec);
+                    Event e = new Event
+                    {
+                        Flagged = false,
+                        Location = Locations[location],
+                        LocationId = Locations[location].LocationId,
+                        TimeStamp = d
+                    };
+                    dailyEvent.Add(d, e);
+                }
+
+                foreach(var de in dailyEvent)
+                {
+                    events.Add(de.Value);
+                }
+
+                eventDate = eventDate.AddDays(1);
+            }
+
+            foreach(Event e in events)
+            {
+                Console.WriteLine($"{e.TimeStamp}, {e.Location.Name}");
+                
+            }
+        
+        }
+    }
+
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Event
+    {
+        public int EventId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Flagged { get; set; }
+        // foreign key for location 
+        public int LocationId { get; set; }
+        // navigation property
+        public Location Location { get; set; }
     }
 }
